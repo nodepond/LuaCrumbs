196b4c87a7ebf6a9ae27ccdc20f3e242ae579736
Martin Wisniowski <mw@Martins-MacBook-Pro.local>
2011-10-18 23:43:45 +0200

    made generation of gcode sort of optional - taking the architecture towards a multi-functional core. NOTICE: changes not tested on a CNC-machine yet

--- particles.lua
+++ particles.lua
@@ -17,9 +17,13 @@ module(..., package.seeall)
 
 -- define some variables for config and control
 particles_circlemode = 0 -- particles_circlemode: 0 = corner, 1 = radius
+
+particles_generate_gcode = true -- generate html output file?
+particles_gcode = "" -- the content, that is added to the html-file
+
 particles_generate_html = true -- generate html output file?
 particles_html = "" -- the content, that is added to the html-file
-particles_filename = ""
+particles_projectname = ""
 
 -- keep track of the current positions (we need this in some occations)
 curx = 0
@@ -31,43 +35,48 @@ html_zoom = 4
 
 function standardInit(verbose)
 	-- this is the standard init
-	if verbose then
-		file:write("G40 (disable tool radius compensation)\n")
-		file:write("G49 (disable_tool_length_compensation)\n")
-		file:write("G80 (cancel_modal_motion)\n")
-		file:write("G54 (select_coordinate_system_1)\n")
-		file:write("G90 (Absolute distance mode)\n") -- "incremental motion"
-		file:write("G21 (metric)\n")
-		file:write("G61 (exact path mode)\n")
-	else -- comment: is there any real reason for shortening and therefore obfuscinating the g.code?!
-		file:write("G40\n")
-		file:write("G49\n")
-		file:write("G80\n")
-		file:write("G54\n")
-		file:write("G90\n")
-		file:write("G21\n")
-		file:write("G61\n")
+		
+	if particles_generate_gcode then
+		particles_gcode = ""
+	
+		if verbose then
+			particles_gcode = particles_gcode.."G40 (disable tool radius compensation)\n"
+			particles_gcode = particles_gcode.."G49 (disable_tool_length_compensation)\n"
+			particles_gcode = particles_gcode.."G80 (cancel_modal_motion)\n"
+			particles_gcode = particles_gcode.."G54 (select_coordinate_system_1)\n"
+			particles_gcode = particles_gcode.."G90 (Absolute distance mode)\n" -- "incremental motion"
+			particles_gcode = particles_gcode.."G21 (metric)\n"
+			particles_gcode = particles_gcode.."G61 (exact path mode)\n"
+		else -- comment: is there any real reason for shortening and therefore obfuscinating the g-code?!
+			particles_gcode = particles_gcode.."G40\n"
+			particles_gcode = particles_gcode.."G49\n"
+			particles_gcode = particles_gcode.."G80\n"
+			particles_gcode = particles_gcode.."G54\n"
+			particles_gcode = particles_gcode.."G90\n"
+			particles_gcode = particles_gcode.."G21\n"
+			particles_gcode = particles_gcode.."G61\n"
+		end
 	end
 end
 
 function setSpeed(val)
 	if val == nil or val == 0 then
-		file:write("G0\n")
+		particles_gcode = particles_gcode.."G0\n"
 		return
 	end
 	if val > 0 then
-		file:write("F"..val.."\n")
+		particles_gcode = particles_gcode.."F"..val.."\n"
 	end
 end
 
 -- Move to absolute coordinate (every parameter is optional. If nil, the xyz value is unchanged)
 -- TODO: Please test this with only one or zero parameters with an g-code device
 function moveTo(xpos, ypos, zpos)
-	file:write("G1")
-	if xpos ~= nil then file:write(" x"..xpos) end
-	if ypos ~= nil then file:write(" y"..ypos) end
-	if zpos ~= nil then file:write(" z"..zpos) end
-	file:write("\n")
+	particles_gcode = particles_gcode.."G1"
+	if xpos ~= nil then particles_gcode = particles_gcode.." x"..xpos end
+	if ypos ~= nil then particles_gcode = particles_gcode.." y"..ypos end
+	if zpos ~= nil then particles_gcode = particles_gcode.." z"..zpos end
+	particles_gcode = particles_gcode.."\n"
 	
 	-- update the current position
 	if xpos ~= nil then curx = xpos end
@@ -78,9 +87,9 @@ end
 -- Draw circle, counterclockwise-parameter is optional
 function circle(radius, counterclockwise)
 	if counterclockwise == true then
-		file:write("G3 J"..radius.."\n")
+		particles_gcode = particles_gcode.."G3 J"..radius.."\n"
 	else
-		file:write("G2 J"..radius.."\n")
+		particles_gcode = particles_gcode.."G2 J"..radius.."\n"
 	end
 	
 	if particles_generate_html then
@@ -110,14 +119,12 @@ function pause(seconds)
 			return
 		end
 	end
-	file:write("G4 P"..seconds.."\n")
+	particles_gcode = particles_gcode.."G4 P"..seconds.."\n"
 end
 -- verbose = true or false
-function init(output_file, verbose)
-	if output_file == nil then output_file = "output.txt" end
-	particles_filename = output_file
-	file = io.open(output_file, "w+")
-	print("\nOpened file: "..output_file)
+function init(projectname, verbose)
+	if projectname == nil then projectname = "particles-project" end
+	particles_projectname = projectname
 	
 	particles_circlemode = 1 -- set circlemode to radius, not corner
 	
@@ -126,11 +133,11 @@ function init(output_file, verbose)
 end
 
 -- universal function to write something into the gcode file, i.e. custom commands like "M101"
-function writeToFile(string)
-	file:write(string.."\n")	
+function insertIntoGCodeFile(string)
+	particles_gcode = particles_gcode..string.."\n"
 end
 
---- Should Particles generate an additional html5-file, that visualises the content?
+-- Should Particles generate an additional html5-file, that visualises the content?
 -- The generated file can be opened in any html5-canvas ready browser, to preview the output if the g-code
 -- @params flag: true or false
 function generateHTML(flag)
@@ -139,7 +146,7 @@ end
 
 function doGenerateHTML()
 		print("\nStarting to write html-file.")
-		output_file = particles_filename..".html"
+		output_file = particles_projectname..".html"
 		file = io.open(output_file, "w+")
 		print("Opened file: "..output_file)	
 		
@@ -161,11 +168,29 @@ function doGenerateHTML()
 		print("Writing of "..output_file.." complete.\n")	
 end
 
-function close()
-	file:write("M2\n")
-	file:close()
-	print("Writing of file complete.\n")
-	
+-- Should Particles generate a ngc-file (G-Code). The default is yes, since it is intended to use Particles with G-Code!
+-- @params flag: true or false
+function generateGCode(flag)
+	particles_generate_gcode = flag
+end
+
+function doGenerateGCode()
+		print("\nStarting to write GCode-file.")
+		output_file = particles_projectname..".ngc"
+		file = io.open(output_file, "w+")
+		print("Opened file: "..output_file)			
+		file:write(particles_gcode)
+		file:close()
+		print("Writing of "..output_file.." complete.\n")	
+end
+
+
+function close()	
+	if particles_generate_gcode then		
+		particles_gcode = particles_gcode.."M2\n" -- is this call maybe in the wrong place?
+		doGenerateGCode()
+	end
+		
 	if particles_generate_html then
 		doGenerateHTML()
 	end
@@ -203,6 +228,10 @@ end
 function pencilDown()
 	moveToZPosition(particles_pencildownpos)
 end
+
+-- NOTICE: line and lineTo are still problematic, because tey take move-to into account, but are not always "drawn". 
+-- This is a little ugly dependency, also that there is an 'if particles_generate_html then'-statement at this place. 
+-- This kind of statements should be only in the Core!!
 
 -- draw line: mode to up, go to start pos, move down, draw line to dest
 function line(xstart, ystart, xdest, ydest)
